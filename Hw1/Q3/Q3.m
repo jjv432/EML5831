3793a032b6b82cc41a8874d031c918ee6ab866c0
jjv432 <jackvranicar43@gmail.com>
2024-09-23 16:05:52 -0400

    Improved comments

--- Hw1/Q3/Q3.m
+++ Hw1/Q3/Q3.m
@@ -18,10 +18,12 @@ netconf = [10 10]; %two layers
 
 net = feedforwardnet(netconf);
 
+% Trainig the network
 net = train(net, x_train.X, y_train.Y); 
 
 %% 3.2 
 
+% Organizing testing data
 theta_dot_r_test = x_test.Xtest(1,:);
 theta_dot_l_test = x_test.Xtest(2,:);
 temp_test = x_test.Xtest(3,:);
@@ -29,11 +31,15 @@ temp_test = x_test.Xtest(3,:);
 vehicle_velo_measured = y_test.Ytest(1,:);
 vehicle_omega_measured = y_test.Ytest(2,:);
 
+% Making a prediction based on network and organizing values
 vehicle_predicted = net(x_test.Xtest);
 
 vehicle_velo_predicted = vehicle_predicted(1,:);
 vehicle_omega_predicted = vehicle_predicted(2,:);
 
+%% Plotting
+
+% Test v predicted Velocity
 figure()
     plot3(theta_dot_r_test, theta_dot_l_test, vehicle_velo_measured, 'x');
     hold on
@@ -43,6 +49,7 @@ figure()
     ylabel("\omega_l")
     zlabel("Velocity")
 
+% Test v predicted Angular Velocity
 figure()
     plot3(theta_dot_r_test, theta_dot_l_test, vehicle_omega_measured, 'x');
     hold on
