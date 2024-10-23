70aae4be42a9acf61479442abd5d5b9bafe211e1
jjv432 <jackvranicar43@gmail.com>
2024-10-22 21:30:17 -0400

    Complementary filter; formatting

--- Exam1/Question1/Q1.m
+++ Exam1/Question1/Q1.m
@@ -1,8 +1,8 @@
-%clearvars -except Dynamic StaticA StaticB StaticC
+clearvars -except Dynamic StaticA StaticB StaticC
 close all
 clc
 %% Load the data
-dataBool = 1;
+dataBool = 0;
 
 if dataBool
 
@@ -45,7 +45,7 @@ plot(StaticA.Time, StaticA.Acceleration.X)
 plot(StaticA.Time, StaticA.Acceleration.Y)
 plot(StaticA.Time, StaticA.Acceleration.Z)
 legend("X", "Y", "Z")
-title("Static A")
+title("Static A Accelerometer")
 
 figure()
 hold on
@@ -53,7 +53,7 @@ plot(StaticB.Time, StaticB.Acceleration.X)
 plot(StaticB.Time, StaticB.Acceleration.Y)
 plot(StaticB.Time, StaticB.Acceleration.Z)
 legend("X", "Y", "Z")
-title("Static B")
+title("Static B Accelerometer")
 
 figure()
 hold on
@@ -61,7 +61,7 @@ plot(StaticC.Time, StaticC.Acceleration.X)
 plot(StaticC.Time, StaticC.Acceleration.Y)
 plot(StaticC.Time, StaticC.Acceleration.Z)
 legend("X", "Y", "Z")
-title("Static C")
+title("Static C Accelerometer")
 
 % Body Rates
 figure()
@@ -70,15 +70,15 @@ plot(StaticA.Time, StaticA.Orientation.X)
 plot(StaticA.Time, StaticA.Orientation.Y)
 plot(StaticA.Time, StaticA.Orientation.Z)
 legend("X", "Y", "Z")
-title("Static A")
+title("Static A Gyro")
 
 figure()
 hold on
 plot(StaticB.Time(1:(end-1)), StaticB.Orientation.X)
 plot(StaticB.Time(1:(end-1)), StaticB.Orientation.Y)
 plot(StaticB.Time(1:(end-1)), StaticB.Orientation.Z)
-legend("X", "Y", "Z")
-title("Static B")
+    legend("X", "Y", "Z")
+title("Static B Gyro")
 
 figure()
 hold on
@@ -86,7 +86,62 @@ plot(StaticC.Time(1:(end-2)), StaticC.Orientation.X)
 plot(StaticC.Time(1:(end-2)), StaticC.Orientation.Y)
 plot(StaticC.Time(1:(end-2)), StaticC.Orientation.Z)
 legend("X", "Y", "Z")
-title("Static C")
+title("Static C Gyro")
 
 %% 1 B
 
+StaticAAccelX = mean(StaticA.Acceleration.X);
+StaticAAccelY = mean(StaticA.Acceleration.Y);
+StaticAAccelZ = mean(StaticA.Acceleration.Z);
+
+StaticBAccelX = mean(StaticB.Acceleration.X);
+StaticBAccelY = mean(StaticB.Acceleration.Y);
+StaticBAccelZ = mean(StaticB.Acceleration.Z);
+
+StaticCAccelX = mean(StaticC.Acceleration.X);
+StaticCAccelY = mean(StaticC.Acceleration.Y);
+StaticCAccelZ = mean(StaticC.Acceleration.Z);
+
+thetaA = (180/pi) * atan2(StaticAAccelY, StaticAAccelZ);
+thetaB = (180/pi) * atan2(StaticBAccelY, StaticBAccelZ);
+thetaC = (180/pi) * atan2(StaticCAccelY, StaticCAccelZ);
+
+%% 1 C
+
+% Angle[k] = [theta_dot_gyro * dt + angle_[k-1]]G + theta_accel *
+% (1-G);
+temp = 330;
+DynamicOrientationX = Dynamic.Orientation.X(1:temp);
+DynamicOrientationY = Dynamic.Orientation.Y(1:temp);
+DynamicOrientationZ = Dynamic.Orientation.Z(1:temp);
+
+figure()
+hold on
+plot(DynamicOrientationX)
+plot(DynamicOrientationY)
+plot(DynamicOrientationZ)
+%legend("X", "Y", "Z")
+
+Angle(1) = thetaA;
+dt = Dynamic.Time(2) - Dynamic.Time(1);
+
+Gs = [.99 .93 .82];
+
+for t = 1:length(Gs)
+
+    G = Gs(t);
+
+for k = 2:length(DynamicOrientationY)
+
+    Angle(k) = (DynamicOrientationY(k) * dt + Angle(k-1)) * G + Dynamic.Acceleration.Z(k) * (1-G);
+   
+
+end
+
+figure()
+plot(Dynamic.Time(1:k), -Angle)
+title(strcat("Angle v Time for K = ", num2str(G)))
+xlabel("time (s)")
+ylabel("\theta (deg)")
+
+end
