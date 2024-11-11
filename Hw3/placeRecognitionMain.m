65762e3b774426a63dbcc7746d8dd638a4dcfb24
jjv432 <jackvranicar43@gmail.com>
2024-11-11 14:24:23 -0500

    Jacobian set up, not using equation correctly

--- Hw3/placeRecognitionMain.m
+++ Hw3/placeRecognitionMain.m
@@ -11,47 +11,65 @@ x_vals = 1:size(COE2, 1);
 y_vals = 1:size(COE2, 2);
 
 
-intensityFunction = @(x, y) ((2 * COE2(x, y, 1) + COE2(x, y, 2) + 3* COE2(x, y, 3))/6);
-f = @(x)[x(1)^2 + x(2)^2, x(1)^3.*x(2)^3];
-A = [1 1]
-
-% [x, ~, ~, ~, ~, ~, jac] = lsqnonlin(intensityFunction, [1 1] ,[],[], optimset('MaxFunEvals', 0, 'MaxIter', 0));
-[x, ~, ~, ~, ~, ~, jac] = lsqnonlin(f, A,[],[], optimset('MaxIter', 0, 'MaxFunEvals', 0));
-jac
-
 intensityMap = getIntensity(COE2);
+
+[Tx, Ty] = gradient(intensityMap);
+
+% The jacobian is the transpose of the gradient
+Jx = transpose(Tx);
+Jy = transpose(Ty);
+
+A = 0; 
+B = 0; 
+C = 0; 
+D = 0;
+
+edgeIndices = [];
+
+for u = 1:length(x_vals)
+    for v = 1:length(y_vals)
+        A = (Jx(v, u))^2;
+        B = Jx(v, u) * Jy(v, u);
+        C = Jy(v, u) * Jx(v, u);
+        D = (Jy(v, u))^2;
+        stucutreMatrix = [A, B; C, D];
+
+        if D > .02
+            edgeIndices = [edgeIndices; u, v];
+        end
+    end
+end
+
+figure()
+imshow(COE2);
+hold on
+plot(edgeIndices, 'rx', 'MarkerSize', 10)
 
 figure
 surfc(intensityMap);
 title("Itensity Image");
 
-J = jacobian(intensityMap, 1:size(COE2, 1));
 
-% function intensityMap = getIntensity(image)
-% 
-% % https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-% x_length = size(image, 1);
-% y_length = size(image, 2);
-% 
-% intensityMap = zeros(x_length, y_length);
-% 
-% for x = 1:x_length
-%     for y = 1:y_length
-%         RGB = image(x, y, :);
-% 
-%         R = RGB(1);
-%         G = RGB(2);
-%         B = RGB(3);
-% 
-%         % intensityMap(x,y) = (R+R+B+G+G+G)/6;
-%         % intensity = (R+R+B+G+G+G)/6;
-% 
-%         f = @(R, G , B) (R+R+B+G+G+G)/6;
-% 
-%         [x, ~, ~, ~, ~, ~, jac] = lsqnonlin(f, [R, G, B] ,[],[], optimset('MaxFunEvals', 0), optimset('MaxIter', 0));
-%         jac
-%     end
-% end
-% 
-% 
-% end
+
+function intensityMap = getIntensity(image)
+
+% https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+x_length = size(image, 1);
+y_length = size(image, 2);
+
+intensityMap = zeros(x_length, y_length);
+
+for x = 1:x_length
+    for y = 1:y_length
+        RGB = image(x, y, :);
+
+        R = RGB(1);
+        G = RGB(2);
+        B = RGB(3);
+        intensity = (R+R+B+G+G+G)/6;
+        intensityMap(x,y) = intensity;
+
+    end
+end
+
+end
